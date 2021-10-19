1f744a4ee34c18a01435dea441109df0c44ddbdc
Chanchaljangir <chanchaljangir9@gmail.com>
2021-10-20 00:32:30 +0530

    view

--- project-fe/src/app/student/view-student/view-student.component.ts
+++ project-fe/src/app/student/view-student/view-student.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ToasterService } from 'angular2-toaster';
+import { StudentServiceService } from '../service/student-service.service';
+declare var $;
 @Component({
   selector: 'app-view-student',
   templateUrl: './view-student.component.html',
   styleUrls: ['./view-student.component.css']
 })
 export class ViewStudentComponent implements OnInit {
+  loading: boolean;
+  stuData: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private studentService: StudentServiceService, private ts: ToasterService,) {
   }
 
+  ngOnInit() {
+    this.getAllStudents()
+  }
+  getAllStudents() {
+    this.studentService.getAllStudent().subscribe((res: any) => {
+      this.loading = false;
+      if (res.IsSuccess) {
+        this.loading = false;
+        this.stuData = res.Data;
+      } else {
+        this.loading = false;
+        console.log("something wents wrong ");
+        this.ts.pop("error", "", "data not fetched");
+      }
+    });
+  }
 }
